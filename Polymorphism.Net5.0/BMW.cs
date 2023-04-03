997023917332bcb6ab01b84fa64aeee0fadf396c
Timothy Axford <t.axford2@yahoo.com>
2023-04-03 12:58:23 -0400

    Created an Audi class

--- Polymorphism.Net5.0/BMW.cs
+++ Polymorphism.Net5.0/BMW.cs
@@ -9,13 +9,23 @@ namespace Polymorphism.Net5._0
     internal class BMW : Car
     {
         //property
-        private string Brand = "BMW";
+        private string brand = "BMW";
         public string Model { get; set; }
 
         //constructor
         public BMW(int hp, string color, string model):base(hp, color)
         {
             this.Model = Model;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("Brand " + brand + "HP: " + HP + " color: " + Color);
+        }
+
+        public void Repair()
+        {
+            Console.WriteLine("Tje BMW {0} was repaired", Model);
         }
     }
 }
