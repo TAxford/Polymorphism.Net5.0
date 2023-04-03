ee85890a8cf6e4962f37a68ad17b77dbadcffda9
Timothy Axford <t.axford2@yahoo.com>
2023-04-03 12:59:37 -0400

    Copied all information from BMW to Audi and fixed the errors

--- Polymorphism.Net5.0/Audi.cs
+++ Polymorphism.Net5.0/Audi.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Polymorphism.Net5._0
 {
-    internal class Audi
-    {
+    internal class Audi : Car
+    {//property
+        private string brand = "Audi";
+        public string Model { get; set; }
+
+        //constructor
+        public Audi(int hp, string color, string model) : base(hp, color)
+        {
+            this.Model = Model;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("Brand " + brand + "HP: " + HP + " color: " + Color);
+        }
+
+        public void Repair()
+        {
+            Console.WriteLine("Tje BMW {0} was repaired", Model);
+        }
     }
 }
