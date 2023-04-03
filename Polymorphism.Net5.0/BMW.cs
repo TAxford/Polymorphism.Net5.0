fcb66b6789346bcbe6075f436bad7a801ae6f171
Timothy Axford <t.axford2@yahoo.com>
2023-04-03 12:55:40 -0400

    Created a Constructor that uses the base constructor fromt he base class

--- Polymorphism.Net5.0/BMW.cs
+++ Polymorphism.Net5.0/BMW.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace Polymorphism.Net5._0
 {
-    internal class BMW
+    internal class BMW : Car
     {
         //property
-        private string brand = "BMW";
+        private string Brand = "BMW";
         public string Model { get; set; }
+
+        //constructor
+        public BMW(int hp, string color, string model):base(hp, color)
+        {
+            this.Model = Model;
+        }
     }
 }
